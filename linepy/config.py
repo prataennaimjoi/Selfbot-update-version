dd533bcf5299cc1f8d8bef2897ce3dda7e42c842
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-26 15:39:20 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -31,7 +31,7 @@ class Config(object):
     APP_VER     = '7.18.1'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'ptatan1983'
-    SYSTEM_VER  = '11.16.2'
+    SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
