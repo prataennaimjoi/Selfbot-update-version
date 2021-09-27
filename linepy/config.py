17c0f3d4a6077f0986b9549c0f3e3827f16cbcae
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-27 09:18:06 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,7 +27,7 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES['IOSIPAD']
     APP_VER     = '6.1.2'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'ptatan1983'
