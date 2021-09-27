366d1239c0bd57d0dde5bcaa9e736f91b3983404
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-27 09:09:15 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -28,7 +28,7 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '7.18.1'
+    APP_VER     = '6.1.2'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'ptatan1983'
     SYSTEM_VER  = '11.16.2'
