6570f88d9fbaf68ba64508036445d992598cc82f
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-27 11:31:27 +0700

    Add files via upload

--- linepy/config.py
+++ linepy/config.py
@@ -27,11 +27,11 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[305]
-    APP_VER     = '7.18.1'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'ptatan1983'
-    SYSTEM_VER  = '11.16.2'
+    SYSTEM_NAME = 'Nadya'
+    SYSTEM_VER  = '12.1.1'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
