64d9f132059e45ac61f4281a6beddf0bdf2f59a9
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-28 08:36:53 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -47,7 +47,7 @@ class Config(object):
     APP_TYPE    = 'CHROMEOS'
     APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Mbah Google'
+    SYSTEM_NAME = 'ptatan1983'
     SYSTEM_VER  = '11.16.2'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
